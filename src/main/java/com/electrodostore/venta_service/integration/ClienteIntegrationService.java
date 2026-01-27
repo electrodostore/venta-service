ab59d2e3f3a7592f5b51e9a7741f5e96786aaec9
Emanuel <emanuelatencia1126654@correo.itm.edu.co>
2026-01-27 10:59:24 -0500

    Métodos de integración protegidos con Circuit-Breaker para integración con cliente-service y producto-service

---
+++ src/main/java/com/electrodostore/venta_service/integration/ClienteIntegrationService.java
@@ -0,0 +1,49 @@
+package com.electrodostore.venta_service.integration;
+
+import com.electrodostore.venta_service.client.ClienteFeignClient;
+import com.electrodostore.venta_service.dto.ClienteIntegrationDto;
+import com.electrodostore.venta_service.exception.ClienteNotFoundException;
+import com.electrodostore.venta_service.exception.ServiceUnavailable;
+import feign.FeignException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+//@Slf4j contiene un logger para notificar errores y eventos en el log del proyecto
+@Slf4j
+@Service
+public class ClienteIntegrationService {
+
+    //Inyección de dependencia por constructor para el FeignClient del servicio Cliente
+    private final ClienteFeignClient clienteFeign;
+
+    public ClienteIntegrationService(ClienteFeignClient clienteFeign) {
+        this.clienteFeign = clienteFeign;
+    }
+
+    //Protección por Circuit-Breaker del método que integra el servicio Cliente con el servicio Venta para buscar un cliente pedido desde la veta
+    //Error de algún tipo en la comunicación -> "plan-B" = fallbackMethod
+    //@Retry = reintentar una cierta contidad de veces antes de ir al fallback
+    @CircuitBreaker(name = "cliente-service", fallbackMethod = "fallbackClienteService")
+    @Retry(name = "cliente-service")
+    public ClienteIntegrationDto findCliente(Long clienteId) {
+        return clienteFeign.foundCliente(clienteId);
+    }
+
+    //El fallback debe tener la misma firma del método protegido, además del último parámetro Throwable
+    public ClienteIntegrationDto fallbackClienteService(Long clienteId, Throwable ex) {
+
+        //Lanzamos el warning al log del proyecto indicando la activación del fallback
+        log.warn("Fallback activado para clienteId={}", clienteId + " en cliente-service", ex);
+
+        //Si la excepción es porque no se pudo encontrar un cliente -> Excepción personalizada NOT_FOUND
+        if (ex instanceof FeignException.NotFound) {
+            throw new ClienteNotFoundException("No se encontró cliente con id: " + clienteId);
+        }
+
+        //Si la excepción no es NOT_FOUND, entonces estamos hablando de una excepción de infraestructura en la comunicación de los servicios -> ServiceUnavailable
+        throw new ServiceUnavailable("No se logró establecer la comunicación con cliente-service. Por favor intente más tarde");
+
+    }
+}
---
+++ src/main/java/com/electrodostore/venta_service/integration/ProductoIntegrationService.java
@@ -0,0 +1,54 @@
+package com.electrodostore.venta_service.integration;
+
+import com.electrodostore.venta_service.client.ProductoClient;
+import com.electrodostore.venta_service.dto.ProductoIntegrationDto;
+import com.electrodostore.venta_service.exception.ProductoNotFoundException;
+import com.electrodostore.venta_service.exception.ServiceUnavailable;
+import feign.FeignException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+//Slf4j provee un logger para establecer eventos y errores en el log del proyecto
+@Slf4j
+@Service
+public class ProductoIntegrationService {
+
+    //Inyección de dependencia por constructor para el cliente de producto-service
+    private final ProductoClient productoClient;
+    public ProductoIntegrationService(ProductoClient productoClient){
+        this.productoClient = productoClient;
+    }
+
+    //Método protegido con Circuit-Breaker para acceder como cliente al servicio producto
+    //Si hay algún problema en la comunicación con producto-service o si el circuito está: open -> fallbackMethod
+    //El fallbackMethod sirve como plan-B en caso de alguna excepción en la ejecución de este método
+    /*En caso de error el @Retry reintenta la petición un determinado número de veces para no ir directamente al
+    fallbackMethod sino darle como otras oportunidades a la petición de realizarse*/
+    @CircuitBreaker(name = "producto-service", fallbackMethod = "fallbackProductoService")
+    @Retry(name = "producto-service")
+    public List<ProductoIntegrationDto> findProductos(Set<Long> productsIds){
+        return productoClient.findProductos(new ArrayList<>(productsIds));
+    }
+
+    //Definimos fallbackMethod que tiene que tener la misma firma que el método protegido
+    //Además su último parámetro debe ser un objeto Throwable que es básicamente la excepción que lo provocó
+    public List<ProductoIntegrationDto> fallbackProductoService(Set<Long> productsIds, Throwable ex){
+
+        //Agregamos le warn al log indicando que el fallback fue activado por problema de comunicación
+        log.warn("fallback activado para producto-service", ex);
+
+        //Si la excepción es NOT_FOUND, ya se sabe que es porque no se encontró ningún producto, entonces lo indicamos
+        if(ex instanceof FeignException.NotFound){
+            throw new ProductoNotFoundException("No se encontró ningún producto solicitado");
+        }
+
+        //Si la excepción no es NOT_FOUND entonces ya es un error en la comunicación con el servicio producto -> Lo indicamos
+        throw new ServiceUnavailable("No se pudo establecer comunicación con producto-service. Por favor intente más tarde");
+    }
+}
