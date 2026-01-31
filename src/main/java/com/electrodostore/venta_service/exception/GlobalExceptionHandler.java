67698a5f3be556afea4a2239dcdea73a6ba281ac
Emanuel <emanuelatencia1126654@correo.itm.edu.co>
2026-01-31 10:06:28 -0500

    Agregación del errorCode en la response de cada excepción

--- src/main/java/com/electrodostore/venta_service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/electrodostore/venta_service/exception/GlobalExceptionHandler.java
@@ -15,13 +15,14 @@ a ver si existe un handler que la maneje, si no lanza un handler por defecto*/
 public class GlobalExceptionHandler {
 
     //Método propio para construir el mensaje de error de cada excepción personalizada
-    private Map<String, Object> buildErrorMessage(HttpStatus status, String message){
+    private Map<String, Object> buildErrorMessage(HttpStatus status, String message, String errorCode){
         Map<String, Object> response = new LinkedHashMap<>();
 
         //Establecemos pares: clave-valor para construir la response
         response.put("timestamp", LocalDateTime.now());
         response.put("status", status.value());
         response.put("error", status.getReasonPhrase());
+        response.put("errorCode", errorCode);
         response.put("mensaje", message);
 
         return response;
@@ -31,28 +32,36 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(VentaNotFoundException.class)
     public ResponseEntity<Map<String, Object>> handlerVentaNotFound(VentaNotFoundException ex){
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(buildErrorMessage(HttpStatus.NOT_FOUND, ex.getMessage()));
+                .body(buildErrorMessage(HttpStatus.NOT_FOUND, ex.getMessage(), ex.getErrorCode().name()));
     }
 
     //Manejador de la excepción ProductoNotFound
     @ExceptionHandler(ProductoNotFoundException.class)
     public ResponseEntity<Map<String, Object>> handlerProductoNotFound(ProductoNotFoundException ex){
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(buildErrorMessage(HttpStatus.NOT_FOUND, ex.getMessage()));
+                .body(buildErrorMessage(HttpStatus.NOT_FOUND, ex.getMessage(), ex.getErrorCode().name()));
     }
 
     //Manejador de la excepción ClienteNotFound
     @ExceptionHandler(ClienteNotFoundException.class)
     public ResponseEntity<Map<String, Object>> handlerClienteNotFound(ClienteNotFoundException ex){
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(buildErrorMessage(HttpStatus.NOT_FOUND, ex.getMessage()));
+                .body(buildErrorMessage(HttpStatus.NOT_FOUND, ex.getMessage(), ex.getErrorCode().name()));
     }
 
     //Handler de la excepción personalizada ServiceUnavailable
     @ExceptionHandler(ServiceUnavailable.class)
     public ResponseEntity<Map<String, Object>> handlerServiceUnavailable(ServiceUnavailable ex){
+        Map<String, Object> response = new LinkedHashMap<>();
+
+        //Establecemos pares: clave-valor para construir la response
+        response.put("timestamp", LocalDateTime.now());
+        response.put("status", HttpStatus.SERVICE_UNAVAILABLE.value());
+        response.put("error", HttpStatus.SERVICE_UNAVAILABLE.getReasonPhrase());
+        response.put("mensaje", ex.getMessage());
+
         return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
-                .body(buildErrorMessage(HttpStatus.SERVICE_UNAVAILABLE, ex.getMessage()));
+                .body(response);
     }
 
 }
