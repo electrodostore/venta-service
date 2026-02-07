19fa3eb332ca96dc25ce87015473b8c076c59dd4
Emanuel <emanuelatencia1126654@correo.itm.edu.co>
2026-02-07 08:07:36 -0500

    Annotation @JsonIgnoreProperties(ignoreUnkown=true) para que al deserealizar un Json me ignore los campos que no pueden ser recibidos en el dto

--- src/main/java/com/electrodostore/venta_service/integration/common/ErrorResponseDto.java
+++ src/main/java/com/electrodostore/venta_service/integration/common/ErrorResponseDto.java
@@ -1,11 +1,14 @@
 package com.electrodostore.venta_service.integration.common;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter  @Setter
 /*Clase DTO para transferencia de los datos del body (JSON) de la Response de un servicio de integración a objeto Java siempre y
 cuando el statusCode de esta sea diferente a 2xx*/
+@JsonIgnoreProperties(ignoreUnknown = true) /*->Annotation de Jackson para ignorar aquellos campos del body de la Response que no
+son necesarios o son redundantes en este dominio (timestamp, error, status, etc.)*/
 public class ErrorResponseDto {
 
     //Del JSON solo es necesario sacar el errorCode y el mensaje para construir la determinada excepción
