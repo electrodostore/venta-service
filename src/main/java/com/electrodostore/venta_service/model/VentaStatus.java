40e0243d255f110cc658cb7465174e527a6babe6
Emanuel <emanuelatencia2407@gmail.com>
2026-05-23 21:59:42 -0500

    Definir posibles estados de una venta durante su LifeCycle

---
+++ src/main/java/com/electrodostore/venta_service/model/VentaStatus.java
@@ -0,0 +1,9 @@
+package com.electrodostore.venta_service.model;
+
+//Define posibles estados de una venta durante su LifeCycle
+public enum VentaStatus {
+    PENDING,
+    CANCEL,
+    COMPLETED,
+    RETURNED
+}
