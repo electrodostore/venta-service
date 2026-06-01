1a9eee9d23d6b086b979682cde2c4e963c27cb5c
Emanuel <emanuelatencia2407@gmail.com>
2026-06-01 12:55:53 -0500

    feat: implementar control de acceso basado en roles y autenticación

--- src/main/java/com/electrodostore/venta_service/controller/VentaController.java
+++ src/main/java/com/electrodostore/venta_service/controller/VentaController.java
@@ -10,6 +10,7 @@ import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.br.CNPJ;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,27 +25,32 @@ public class VentaController {
         this.ventaService = ventaService;
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
     public ResponseEntity<List<VentaResponseDto>> findAllVentas(){
         return ResponseEntity.ok(ventaService.findAllVentas());
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{id}")
     public ResponseEntity<VentaResponseDto> findVenta(@PathVariable Long id){
         return ResponseEntity.ok(ventaService.findVentaResponse(id));
     }
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/cliente/{clientId}")
     public ResponseEntity<List<VentaResponseDto>> findClienteVentas(@PathVariable Long clientId){
         return ResponseEntity.ok(ventaService.findClienteVentas(clientId));
     }
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping
     public ResponseEntity<VentaCreadaDto> createVenta(@RequestBody @NotEmpty List<@NotNull @Valid ProductoRequestDto> productsList){
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ventaService.saveVenta(productsList));
     }
 
+    @PreAuthorize("isAuthenticated()")
     @PatchMapping("/{id}/cancel")
     public ResponseEntity<Void> cancelVenta(@PathVariable Long id){
         ventaService.cancelVenta(id);
@@ -52,6 +58,7 @@ public class VentaController {
         return ResponseEntity.noContent().build();
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PatchMapping("/{id}/admin/cancel")
     public ResponseEntity<Void> cancelVentaByAdmin(@PathVariable Long id){
         ventaService.cancelVentaByAdmin(id);
